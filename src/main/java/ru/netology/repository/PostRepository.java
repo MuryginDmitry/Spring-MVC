b89138725fb278eaed475baf090d8d5d19258975
DmitryM <tihiydon@list.ru>
2023-09-02 14:06:09 +0300

    first commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,33 @@
+package ru.netology.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+@Repository
+public class PostRepository {
+    private final Map<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong idCounter = new AtomicLong(0);
+
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(idCounter.incrementAndGet());
+        }
+        posts.put(post.getId(), post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.remove(id);
+    }
+}
